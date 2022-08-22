47a0a4a3784f4643a675fefc882092ce2c6d27c5
serhii15yurchenko <serhii15yurchenko@gmail.com>
2022-08-22 19:19:52 +0300

    added all files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const HEADER = document.querySelector ('.header');
+const MENU_BUTTON = HEADER.querySelector ('.menu-button');
+
+MENU_BUTTON.addEventListener('click', function() {
+    HEADER.classList.toggle('header--menu-active')
+    document.body.classList.toggle('no-scroll')
+});
